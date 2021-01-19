e9d8789a6834f10e6f7bfa7b080eab2c411caa9c
Jeremy Fergason <jeremy.fergason@corwynn.com>
2021-01-19 01:01:38 -0700

    Make deploy script actually publish to github

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,13 @@ cd dist
 
 # if you are deploying to a custom domain
 echo 'www.pennyvault.com' > CNAME
-
-#git init
-#git add -A
-#git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
 cd -
+
+# Delete the old build
+rm -rf docs
+mv dist docs
+
+# Publish it
+git add docs
+git commit -m "Publish website"
+git push
